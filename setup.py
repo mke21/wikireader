708e46fb9ac4986516bb6f5f4e78fa94d06f0a44
Marcel <4930603+mke21@users.noreply.github.com>
2020-11-16 20:03:05 +0100

    version 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="wikireader",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     description="Read wikipedia articles distraction free",
     long_description=long_description,
